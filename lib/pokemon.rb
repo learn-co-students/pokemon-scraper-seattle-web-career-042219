94ce86fdec4b6ebd95f1cadc8c36b44e37517f7a
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-30 16:57:39 -0700

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
+
 class Pokemon
+  attr_accessor :name, :type
+  attr_reader :db, :id
+
+  @@all= []
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all<< self
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES(?,?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE pokemon.id = #{id}"
+    array = db.execute(sql).flatten
+    self.new(id:array[0], name:array[1], type:array[2], db:db)
+end
+
+
 end
