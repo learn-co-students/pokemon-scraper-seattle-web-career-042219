bf4b4c87c191ddcbb1fb519b946ada69a3ec2a24
jjsmokovich <JesseSmokovich@yahoo.com>
2019-04-30 17:34:18 -0700

    Jamal

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(args)
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+
+  def self.save(pokemon_name, pokemon_type, db)
+   # binding.pry
+   sql = "INSERT INTO pokemon (name, type)
+      VALUES (?, ?);"
+    db.execute(sql, pokemon_name, pokemon_type)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE pokemon.id = #{id};"
+    # binding.pry
+    info = db.execute(sql).first
+    Pokemon.new({:id => info[0], :name => info[1], :type => info[2] })
+    # db.map {|id| self.new(id)}
+  end
+
+
 end
